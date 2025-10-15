de731b4f05438e036864c5a6a8751ed9fdd76c7d
Taras <tarasostapchuk27@gmail.com>
2025-10-15 20:28:50 +0200

    Create search form for Drivers, Cars and Manufacturers pages

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "crispy_forms",
+    "crispy_bootstrap4",
     "taxi",
 ]
 
@@ -80,6 +81,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "taxi_service.wsgi.application"
 
+CRISPY_TEMPLATE_PACK="bootstrap4"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
